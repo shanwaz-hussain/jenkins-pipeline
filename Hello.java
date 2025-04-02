7dee40be71c33c2442edcba42aabda4036ec298c
Shanwaz Hussain <114252393+shanwaz-hussain@users.noreply.github.com>
2025-04-02 17:18:30 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello
 	public static void main(String []args){
 		System.out.println("Hello Shanwaz...");
 		System.out.println("changed now");
-System.out.println("just now changed");
+System.out.println("just now changed code");
 
 	}
 }
